6f9faf355b8188eda0a911cab9ec9a00e1df0927
marco villanueva <villanuevamarco98@gmail.com>
2020-07-21 20:40:12 -0600

    Add project files.

---
+++ LearningNulls/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LearningNulls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Null coalescing allows to deal with possibly
+            //having Nulls
+            String s = null; //"hello";
+            //you get the default string since first string is Null
+            var s2 = s ?? "default";
+
+            Console.Write(s2)
+        }
+    }
+}
